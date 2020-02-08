731979db9291928d7cb02ef96c7d090d3634bf95
Dan Foley <dan@dan-foley.com>
2020-02-08 16:38:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,3 +13,4 @@ binding.pry
 doc.css(".headline-26OIBN")
 
 doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+#\33 c239880-48cf-5ce7-84d0-578546d87586 > div > div.content-3OTK_R > div > div:nth-child(1)
