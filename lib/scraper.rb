4809f9db9f710e493fdc4f9279a6da930d3fcb0c
Dan Foley <dan@dan-foley.com>
2020-02-08 16:50:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,6 +14,8 @@ doc.css(".headline-26OIBN")
 
 courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 
+binding.pry
+
 courses.each do |course|
   puts course.text.strip
 end
