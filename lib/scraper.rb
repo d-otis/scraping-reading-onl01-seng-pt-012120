d502db3c424e8e6a7ced97ab5adc4310108746e7
Dan Foley <dan@dan-foley.com>
2020-02-08 16:38:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,5 +12,7 @@ binding.pry
 
 doc.css(".headline-26OIBN")
 
-doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 #\33 c239880-48cf-5ce7-84d0-578546d87586 > div > div.content-3OTK_R > div > div:nth-child(1)
+
+3c239880-48cf-5ce7-84d0-578546d87586
